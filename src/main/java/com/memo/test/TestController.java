53b61f41e92b7a4cffeffaf5f977f5f2a2f9aaf2
F10\User <timxxz@naver.com>
2022-10-17 21:30:42 +0900

    초기환경세팅

---
+++ src/main/java/com/memo/test/TestController.java
@@ -0,0 +1,33 @@
+package com.memo.test;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.memo.test.dao.TestDAO;
+
+@Controller
+
+public class TestController {
+	@ResponseBody
+	@RequestMapping("/test1")
+    public String helloWorld() {
+        return "Hello world!";
+    }
+	@RequestMapping("/jspTest")
+	public String jsp() {
+		return "test/test";
+	}
+	@Autowired
+	private TestDAO testDAO;
+	@ResponseBody
+	@RequestMapping("/daoTest")
+	public List<Map<String, Object>> dao() {
+		List<Map<String, Object>> result = testDAO.selectTestList();
+		return result;
+	}
+}
